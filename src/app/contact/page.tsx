77f5df3ba896123e2ac27281f5e77665c5d702dd
elchinhumbatov <elchinhumbatov@yahoo.com>
2025-07-14 21:43:39 +0400

    feat: enhance ContactPage form functionality

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,19 +1,46 @@
-'use client'
-import React from 'react';
-import { Button, Form, Input, Textarea } from '@heroui/react';
+"use client";
+import React, { useState } from "react";
+import { addToast, Button, Form, Input, Spinner, Textarea } from "@heroui/react";
 
 
 export default function ContactPage() {
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [sendingForm, setSendingForm] = useState(false)
+
+  const onSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
-    const data = Object.fromEntries(new FormData(e.currentTarget));
-    console.log(data);
-
-    
-    e.currentTarget.reset();
-    // setErrors({});
-    // const result = callServer(data);
+    setSendingForm(true);
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+    fetch(form.action, {
+      method: form.method,
+      body: formData,
+      headers: {
+          'Accept': 'application/json'
+      }
+    })
+    .then(res => {
+      if (res.ok) {
+        // setName(""); setEmail(""); setMessage("");
+        addToast({
+          title: "Request sent successfully",
+          color: "success",
+        });
+      } else {
+        throw new Error("Network response was not ok");
+      }
+    })
+    .catch(error => {
+      console.error("Error sending email:", error);
+      addToast({
+        title: "Failed to send request",
+        description: "Please try again later.",
+        color: "danger",
+      });
+    })
+    .finally(() => setSendingForm(false));
   };
 
   return (
@@ -23,28 +50,33 @@ export default function ContactPage() {
         <div>
           <p className="text-md">
             <b>Phone: </b>
-            <a href='tel:+971543962051' className="text-gray-700 underlined"> +971 54 396 2051</a>
+            <a href="tel:+971543962051" className="text-gray-700 underlined">
+              {" "}
+              +971 54 396 2051
+            </a>
           </p>
         </div>
         <div>
           <p className="text-md">
             <b>Mail: </b>
-            <a href='mailto:support@tango.com' className="text-gray-700"> support@zaurtango.com</a>
+            <a href="mailto:support@tango.com" className="text-gray-700">
+              {" "}
+              support@zaurtango.com
+            </a>
           </p>
         </div>
       </div>
       <div className="py-10">
         <h2 className="text-xl font-medium mb-4">Get in Touch</h2>
-        <Form
-          className="w-full flex flex-col gap-3"
-          onSubmit={onSubmit}
-        >
+        <Form className="w-full flex flex-col gap-3" onSubmit={onSubmit} action='https://formspree.io/f/xrblzdyn' method="post">
           <Input
             isRequired
             errorMessage="Please enter your name"
             label="Name"
             name="name"
-            variant='underlined'
+            variant="underlined"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
           <Input
             isRequired
@@ -52,23 +84,34 @@ export default function ContactPage() {
             label="Email"
             name="email"
             type="email"
-            variant='underlined'
+            variant="underlined"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <Textarea
             isRequired
             errorMessage="Please enter your message"
             label="Message"
-            name='message'
-            variant='underlined'
+            name="message"
+            variant="underlined"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           />
 
-          <div className='flex flex-col md:flex-row gap-4 my-8'>
-            <p className='text-xs text-gray-500 w-full md:w-1/2'>
-              By sending your message, you agree to accept the General Terms and Conditions of Use and that your data will be processed in compliance with the Privacy Policy of Zaur Tango.
+          <div className="flex flex-col md:flex-row gap-4 my-8">
+            <p className="text-xs text-gray-500 w-full md:w-1/2">
+              By sending your message, you agree to accept the General Terms and
+              Conditions of Use and that your data will be processed in
+              compliance with the Privacy Policy of Zaur Tango.
             </p>
-            <div className='w-full md:w-1/2 flex justify-end'>
-              <Button type="submit" variant="bordered" radius='none' className="w-full md:w-[200px] text-gray-500">
-                Submit
+            <div className="w-full md:w-1/2 flex justify-end">
+              <Button
+                type="submit"
+                variant="bordered"
+                radius="none"
+                className="w-full md:w-[200px] text-gray-500"
+              >
+                {sendingForm ? <><Spinner size="sm" color="default" /> <p>Sending...</p></> : 'Submit'}
               </Button>
             </div>
           </div>
@@ -76,4 +119,4 @@ export default function ContactPage() {
       </div>
     </div>
   );
-};
+}
