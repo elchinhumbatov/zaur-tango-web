f9c6b5e857eae0c0a976a3596f88e4b128757706
elchinhumbatov <elchinhumbatov@yahoo.com>
2025-08-26 00:34:03 +0400

    feat: refactor Footer component layout; update structure for better responsiveness and alignment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,30 @@ export default function Footer() {
   return (
     <section>
       <div className="container mx-auto px-5 pt-20 pb-10">
-        <div className="grid grid-cols-12 grid-rows-2 sm:grid-rows-1 gap-8">
+        <div className="flex flex-col sm:flex-row justify-between items-center mb-10 gap-10">
+          <div className='order-2 sm:order-1 text-center sm:text-left'>
+            <p className='mb-2'>CALL US</p>
+            <a href='tel:+971543962051'>+971 54 396 2051</a>
+          </div>
+
+          <div className='order-1 sm:order-2'>
+            <Image src='/img/logo-light.png' alt='logo' width={150} height={50} />
+          </div>
+          
+          <div className='order-3 text-center sm:text-left'>
+            <p className='mb-2'>FOLLOW</p>
+            <div className='flex gap-1'>
+              <a href='https://www.instagram.com/zaurtango/' target='_blank'>
+                <Instagram />
+              </a>
+              &
+              <a href='https://www.instagram.com/zaur.mamedov.902/' target='_blank'>
+                <Instagram />
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* <div className="grid grid-cols-12 grid-rows-2 sm:grid-rows-1 gap-8">
           <div className='col-span-6 sm:col-span-4 order-2 sm:order-1 flex flex-col'>
             <p className='mb-2'>CALL US</p>
             <a href='tel:+971543962051'>+971 54 396 2051</a>
@@ -28,7 +51,7 @@ export default function Footer() {
               </a>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
       <div className="text-center p-5">
         <p>© {new Date().getFullYear()} All rights reserved.</p>
